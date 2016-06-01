5d636951104af50cc0f1bb92e086e6dbaa85f876
Anusha Srikanthan <Anusha Srikanthan>
2016-06-02 00:01:52 +0530

    Initial commit

---
+++ app/src/main/java/com/android/button/MainActivity.java
@@ -0,0 +1,42 @@
+package com.android.button;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int number_click=0;
+    public void submitClick(View view) {
+        number_click++;
+        display(number_click);
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+
+    }
+
+}
